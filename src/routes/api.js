2cd950d418f836e1bf0fa55c231bc50b38d7da83
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-13 23:44:05 +0900

    IDじゃなくて登録した名前を表示するようにした

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,12 +1,18 @@
 const execQuery = (con, res, values) => sql => con.query(sql, values, (e, results) => e ? console.log(e) : res.json(results))
 const getRanking = (con) => (req, res) =>
     execQuery(con, res)(
-        'select * from records\
+        'select p.name, r.kps, r.miss, r.accuracy, r.score\
+        from records as r\
+        join players as p\
+        on p.id = r.player_id\
         order by score desc'
     )
 const getId = (con) => (req, res) => {
     execQuery(con, res, req.params.id)(
-        'select * from records\
+        'select p.name, r.kps, r.miss, r.accuracy, r.score\
+        from records as r\
+        join players as p\
+        on p.id = r.player_id\
         where player_id = ?'
     )
 }
