fa0f9f7eb32665ca4e7e9dbfde88bc5a022d99e9
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-14 11:12:29 +0900

    記録を登録する時に、今のスコアを新しいスコアが上回っていた場合のみ更新するようにした

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,4 +1,4 @@
-const execQuery = (con, res, values) => sql => con.query(sql, values, (e, results) => e ? console.log(e) : res.json(results))
+const execQuery = (con, res, values) => sql => con.query(sql, values, (e, results) => e ? res.json({code: e.code}) : res.json(results))
 const getRanking = (con) => (req, res) =>
     execQuery(con, res)(
         'select p.name, r.kps, r.miss, r.accuracy, r.score\
@@ -19,7 +19,13 @@ const getRecordById = (con) => (req, res) => {
 const postRecord = (con) => (req, res) => {
     const {player_id, kps, miss, accuracy, score} = req.body
     execQuery(con, res, [player_id, kps, miss, accuracy, score])(
-        'insert ignore into records values (?, ?, ?, ?, ?)'
+        'insert into records (player_id, kps, miss, accuracy, score)\
+        values (?, ?, ?, ?, ?)\
+        on duplicate key update\
+        kps = if(score > values(score), kps, values(kps)),\
+        miss = if(score > values(score), miss, values(miss)),\
+        accuracy = if(score > values(score), accuracy, values(accuracy)),\
+        score = if(score > values(score), score, values(score))'
     )
 }
 const postPlayer = (con) => (req, res) => {
@@ -28,7 +34,6 @@ const postPlayer = (con) => (req, res) => {
         'insert ignore into players values (?, ?)'
     )
 }
-
 
 const createRouter = (con) => {
     const express = require('express')
