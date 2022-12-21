fbb0b9c41be0a590d721d374d926f35e9eb0dacb
itc-s21009 <s21009@std.it-college.ac.jp>
2022-12-21 23:05:05 +0900

    権限チェック(/testadmin)を実装

--- src/api.js
+++ src/api.js
@@ -5,6 +5,15 @@ const getSessionId = (req, res) => {
         res.cookie('session_id', sid = uid(24))
     return sid
 }
+const isAdmin = (con) => (req, res) =>
+    con.query(
+        `select *
+         from admins
+         where id = ?`
+        , getSessionId(req, res), (e, data) => {
+            res.json({admin: data.length > 0})
+        }
+    )
 const checkAdmin = (con) => (req, res, next) =>
     con.query(
         `select *
@@ -107,6 +116,7 @@ const createRouter = (con) => {
     router.post('/sentences/edit', checkAdmin(con), editSentence(con))
     router.post('/sentences/delete', checkAdmin(con), deleteSentence(con))
     router.post('/sentences/register', checkAdmin(con), postSentence(con))
+    router.get('/testadmin', isAdmin(con))
 
     return router
 }
