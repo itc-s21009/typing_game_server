bec0506f3d0012b90b90a9a37b78c629d33a15d4
itc-s21009 <s21009@std.it-college.ac.jp>
2023-01-25 20:10:45 +0900

    express-session導入とそれに伴う修正

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const session = require('express-session')
+const MySQLStore = require('express-mysql-session')(session)
 const mysql = require('mysql2')
 const config = require('config')
 const http = require('http')
@@ -7,19 +9,18 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
 const SENTENCE_MAX_LENGTH = 64
-const con = mysql.createConnection(config.get("db"))
+const con = mysql.createConnection(config.get("db")).promise()
 
 module.exports = {
     SENTENCE_MAX_LENGTH: SENTENCE_MAX_LENGTH,
     db: con
 }
 
-const connectToDatabase = () => con.connect(err => console.log(err ? `データベースに接続中にエラー：${err}` : "データベース接続完了"))
 const setupDatabase = () => {
     const log = msg => err => err ? {} : console.log(msg)
-    con.query('create database typing', log('データベースを作成'))
+    con.query('create database if not exists typing').then(log('データベースを作成'))
     con.query(
-        `create table records
+        `create table if not exists records
          (
              id         varchar(256)           not null primary key,
              name       varchar(16)            not null,
@@ -28,23 +29,23 @@ const setupDatabase = () => {
              accuracy   decimal(4, 1) unsigned not null check (accuracy <= 100.0),
              score      int unsigned           not null,
              updated_at datetime default current_timestamp on update current_timestamp
-         )`, log('recordsテーブルを作成')
-    )
+         )`
+    ).then(log('recordsテーブルを作成'))
     con.query(
-        `create table sentences
+        `create table if not exists sentences
          (
              id       int unsigned                    not null primary key auto_increment,
              sentence varchar(${SENTENCE_MAX_LENGTH}) not null unique,
              kana     varchar(${SENTENCE_MAX_LENGTH}) not null unique
-         )`, log('sentencesテーブルを作成')
-    )
+         )`
+    ).then(log('sentencesテーブルを作成'))
     con.query(
-        `create table admins
+        `create table if not exists admins
          (
              id       varchar(256) not null primary key,
              password varchar(256) not null
-         )`, log('adminsテーブルを作成')
-    )
+         )`
+    ).then(log('adminsテーブルを作成'))
 }
 const setupExpress = () => {
     const app = express()
@@ -61,6 +62,13 @@ const setupExpress = () => {
     }))
     app.use(bodyParser.json())
 
+    app.use(session({
+        secret: config.get('secret'),
+        resave: false,
+        saveUninitialized: false,
+        store: new MySQLStore({}, con)
+    }))
+
     const apiRouter = require('./api')
     app.use('/api', apiRouter)
 
@@ -69,8 +77,12 @@ const setupExpress = () => {
     server.listen(port)
 }
 const main = () => {
-    connectToDatabase()
-    setupDatabase()
-    setupExpress()
+    con.connect()
+        .then(() => {
+            console.log("データベース接続完了")
+            setupDatabase()
+            setupExpress()
+        })
+        .catch((err) => console.log(`データベースに接続中にエラー：${err}`))
 }
 main()
